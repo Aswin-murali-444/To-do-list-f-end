e03404159b814d4fce034004a3b867a7b2b0f0d8
Aswin-murali-444 <aswinmurali444@gmail.com>
2025-04-02 09:26:39 +0530

    folder set

--- src/pages/To-do.jsx
+++ src/pages/To-do.jsx
@@ -36,7 +36,7 @@ function ToDo() {
 
   const fetchTasks = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/todos');
+      const response = await fetch('https://to-do-list-b-end-gf50.onrender.com');
       if (response.ok) {
         const data = await response.json();
         setTasks(data);
@@ -55,7 +55,7 @@ function ToDo() {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/todos', {
+      const response = await fetch('https://to-do-list-b-end-gf50.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -77,7 +77,7 @@ function ToDo() {
 
   const handleDeleteTask = async (taskId) => {
     try {
-      await fetch(`http://localhost:5000/api/todos/${taskId}`, { method: 'DELETE' });
+      await fetch(`https://to-do-list-b-end-gf50.onrender.com/${taskId}`, { method: 'DELETE' });
       setTasks(tasks.filter((task) => task._id !== taskId));
     } catch (error) {
       console.error('Error deleting task:', error);
@@ -91,7 +91,7 @@ function ToDo() {
 
   const saveEditedTask = async () => {
     try {
-      const response = await fetch(`http://localhost:5000/api/todos/${editingTaskId}`, {
+      const response = await fetch(`https://to-do-list-b-end-gf50.onrender.com/${editingTaskId}`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
